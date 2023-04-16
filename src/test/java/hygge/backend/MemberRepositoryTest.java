3338fa0c04aced86b7ebb49ce1efb70e41c0720a
Lee Jisu <itis821@ajou.ac.kr>
2023-04-16 23:27:45 +0900

    Test MemberRepository가Null이아님

---
+++ src/test/java/hygge/backend/MemberRepositoryTest.java
@@ -0,0 +1,19 @@
+package hygge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void MemberRepository가Null이아님() {
+        assertThat(memberRepository).isNotNull();
+    }
+}
