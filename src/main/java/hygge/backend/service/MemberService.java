581203a3885c05925e5cdb43f5257c6fc030c6e8
Lee Jisu <itis821@ajou.ac.kr>
2023-04-18 21:07:48 +0900

    Delete unused import

--- src/main/java/hygge/backend/service/MemberService.java
+++ src/main/java/hygge/backend/service/MemberService.java
@@ -4,7 +4,6 @@ import hygge.backend.dto.SignupDto;
 import hygge.backend.entity.Member;
 import hygge.backend.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
