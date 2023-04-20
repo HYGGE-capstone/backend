8cff2d43e72cdf5950c552ff30669da5d9ebb979
Lee Jisu <itis821@ajou.ac.kr>
2023-04-20 23:42:35 +0900

    FIX: EmailConfig에서 잘못 import된 Value 수정

--- src/main/java/hygge/backend/config/EmailConfig.java
+++ src/main/java/hygge/backend/config/EmailConfig.java
@@ -1,16 +1,14 @@
 package hygge.backend.config;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:email.properties")
 public class EmailConfig {
 
     @Value("${mail.smtp.port}")
