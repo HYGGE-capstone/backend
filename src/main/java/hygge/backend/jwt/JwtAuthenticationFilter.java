247fc9591825fdc661f4a8d7addc7963df01d307
Lee Jisu <itis821@ajou.ac.kr>
2023-04-26 19:58:02 +0900

    ADD: UsernamePasswordAuthenticationFilter를 상속받는 JwtAuthenticationFilter 생성

---
+++ src/main/java/hygge/backend/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,6 @@
+package hygge.backend.jwt;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+}
