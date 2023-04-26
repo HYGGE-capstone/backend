0f1d8cb9977b9e41ca80fd4f37bd68684900fc03
Lee Jisu <itis821@ajou.ac.kr>
2023-04-26 19:31:36 +0900

    ADD : SecurityConfig에 정보 추가

--- src/main/java/hygge/backend/config/SecurityConfig.java
+++ src/main/java/hygge/backend/config/SecurityConfig.java
@@ -1,24 +1,34 @@
 package hygge.backend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CorsFilter corsFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 세션 사용 안함
                 .and()
+                .addFilter(corsFilter)  // Security Filter에 등록 / @CrossOrigin (인증 x)
+                .formLogin().disable()  // Form login 안함
+                .httpBasic().disable()
                 .authorizeRequests()
+                // 요청 허용 설정
+
+                // 그 외 모든 요청 허용
                 .anyRequest().permitAll();
 
         return http.build();
