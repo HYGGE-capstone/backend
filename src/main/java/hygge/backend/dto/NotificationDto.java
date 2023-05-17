68870c84a404b3a1ace1931c99901b68c2e02f18
Lee Jisu <itis821@ajou.ac.kr>
2023-05-18 00:52:46 +0900

    ADD : 알림에 열람 여부 추가

--- src/main/java/hygge/backend/dto/NotificationDto.java
+++ src/main/java/hygge/backend/dto/NotificationDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Builder
@@ -15,11 +16,14 @@ import java.time.LocalDateTime;
 public class NotificationDto {
     private String from;
     private String content;
-    private LocalDateTime createdTime;
+    private String createdTime;
+
+    private boolean isOpened;
 
     public NotificationDto(Notification notification) {
         this.from = notification.getFrom();
         this.content = notification.getContent();
-        this.createdTime = notification.getCreatedTime();
+        this.createdTime = notification.getCreatedTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.isOpened = notification.isOpened();
     }
 }
