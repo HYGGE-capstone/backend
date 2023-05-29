884ff73cbd3a2e1ba5712e5946a8a5c2a8a3d33f
Lee Jisu <itis821@ajou.ac.kr>
2023-05-29 13:46:39 +0900

    FEAT : 과목에 속한 팀 조회 관리자 기능 추가

--- src/main/java/hygge/backend/admin/controller/AdminTeamController.java
+++ src/main/java/hygge/backend/admin/controller/AdminTeamController.java
@@ -1,10 +1,16 @@
 package hygge.backend.admin.controller;
 
+import hygge.backend.dto.response.team.TeamResponse;
+import hygge.backend.dto.team.TeamDto;
 import hygge.backend.service.TeamService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Tag(name = "0 관리자 - 팀", description = "팀(관리자) 관련 API 입니다.")
 @RestController
@@ -13,4 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminTeamController {
 
     private final TeamService teamService;
+
+    // 과목에 속한 팀 목록 조회
+    @GetMapping
+    public TeamResponse getTeamList(@RequestParam Long subjectId) {
+        return teamService.searchTeams(subjectId);
+    }
+
+    // 팀 정보 조회
+
+    // 팀 삭제
 }
