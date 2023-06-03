90888fa630cbb59f87635af95d93fd6d9ba43d33
Lee Jisu <itis821@ajou.ac.kr>
2023-06-03 13:50:15 +0900

    ADD : Notice 엔티티 추가

---
+++ src/main/java/hygge/backend/entity/Notice.java
@@ -0,0 +1,20 @@
+package hygge.backend.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notice {
+    @Id
+    @GeneratedValue
+    @Column(name = "NOTICE_ID")
+    private Long id;
+
+    @Column(name = "NOTICE_CONTENT")
+    private String content;
+}
