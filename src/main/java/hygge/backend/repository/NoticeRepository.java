c95143192f4f674e94b83c54c3d2c81a12eb4080
Lee Jisu <itis821@ajou.ac.kr>
2023-06-03 13:53:27 +0900

    ADD : NoticeRepository 추가

---
+++ src/main/java/hygge/backend/repository/NoticeRepository.java
@@ -0,0 +1,9 @@
+package hygge.backend.repository;
+
+import hygge.backend.entity.Notice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+}
